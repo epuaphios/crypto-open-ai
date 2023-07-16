d6abe201dc20eccf7dd6a8ce7d3833df456d4fa8
ognn <ogunozalp1991@gmail.com>
2023-07-16 19:15:08 +0300

    sadas

--- modules/gpt_controller.py
+++ modules/gpt_controller.py
@@ -4,7 +4,7 @@ import datetime
 import matplotlib.pyplot as plt
 from modules.investment_recommendation import get_investment_recommendation
 from modules.historical_getter import get_historical_data
-from modules.current_price  import get_current_price
+from modules.current_price import get_current_price
 from rich.console import Console
 from modules.option import user_option
 
@@ -20,57 +20,40 @@ with open('gpt_api.txt', 'r') as file:
     gpt_api = file.read().strip()
 
 
-
 def get_prices(coin, days, option):
-
-    coin_usd_price = get_current_price(coin+"-USD", headers)
+    coin_usd_price = get_current_price(coin + "-USD", headers)
     eth_usd_price = get_current_price("ETH-USD", headers)
     # Print current prices
-    print(coin+"-USD price:", coin_usd_price)
+    print(coin + "-USD price:", coin_usd_price)
     print("ETH-USD price:", eth_usd_price)
 
-
     api_url = "https://api.pro.coinbase.com/products"
-
 
     end_date = datetime.datetime.now()
     start_date = end_date - datetime.timedelta(int(days))
 
-
-    coin_data = get_historical_data(coin+"-USD", api_url, start_date, end_date)
+    coin_data = get_historical_data(coin + "-USD", api_url, start_date, end_date)
     coin_timestamps = [datetime.datetime.fromtimestamp(int(entry[0])) for entry in coin_data]
     coin_opens = [entry[3] for entry in coin_data]
     coin_highs = [entry[2] for entry in coin_data]
     coin_lows = [entry[1] for entry in coin_data]
     coin_closes = [entry[4] for entry in coin_data]
 
+    user_option(coin, coin_data, coin_opens, coin_highs, coin_lows, coin_closes, gpt_api, option)
+    show_plot(coin_timestamps, coin_closes, coin_lows, coin_highs, days)
 
-    eth_data = get_historical_data("ETH-USD", api_url, start_date, end_date)
-    eth_timestamps = [datetime.datetime.fromtimestamp(int(entry[0])) for entry in eth_data]
-    eth_opens = [entry[3] for entry in eth_data]
-    eth_highs = [entry[2] for entry in eth_data]
-    eth_lows = [entry[1] for entry in eth_data]
-    eth_closes = [entry[4] for entry in eth_data]
-    user_option(coin,coin_data, eth_data, coin_opens, coin_highs, coin_lows, coin_closes, eth_opens, eth_highs, eth_lows, eth_closes,gpt_api, days, option)
-    show_plot(coin_timestamps, coin_closes, coin_lows, coin_highs, eth_timestamps, eth_closes, eth_lows, eth_highs, days)
 
-def show_plot(coin_timestamps, coin_closes, coin_lows, coin_highs, eth_timestamps, eth_closes, eth_lows, eth_highs, days):
-
-    fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(10, 8))
+def show_plot(coin_timestamps, coin_closes, coin_lows, coin_highs, days):
+    fig, (ax1) = plt.subplots(2, 1, figsize=(10, 8))
     ax1.plot(coin_timestamps, coin_closes, color='blue')
     ax1.vlines(coin_timestamps, coin_lows, coin_highs, color='black', linewidth=1)
     ax1.set_title(f"Bitcoin (coin-USD) - {days} Day Historical Data")
     ax1.set_xlabel("Date")
     ax1.set_ylabel("Price (USD)")
-    ax2.plot(eth_timestamps, eth_closes, color='green')
-    ax2.vlines(eth_timestamps, eth_lows, eth_highs, color='black', linewidth=1)
-    ax2.set_title(f"Ethereum (ETH-USD) - {days} Day Historical Data")
-    ax2.set_xlabel("Date")
-    ax2.set_ylabel("Price (USD)")
-    fig.autofmt_xdate()  
+    fig.autofmt_xdate()
     plt.tight_layout()
     plt.show()
 
+
 def get_option(coin, days, option):
     get_prices(coin, days, option)
-   
--- modules/investment_recommendation.py
+++ modules/investment_recommendation.py
@@ -14,8 +14,7 @@ def chat_with_gpt4(prompt, url, headers):
         return completion
 
 
-def get_investment_recommendation(coin, coin_data, eth_data, coin_opens, coin_highs, coin_lows, coin_closes, eth_opens,
-                                  eth_highs, eth_lows, eth_closes, gpt_api, option):
+def get_investment_recommendation(coin, coin_data, coin_opens, coin_highs, coin_lows,coin_closes, gpt_api, option):
     url = "https://api.openai.com/v1/engines/text-davinci-003/completions"
     headers = {
         "Content-Type": "application/json",
@@ -24,10 +23,8 @@ def get_investment_recommendation(coin, coin_data, eth_data, coin_opens, coin_hi
     coin_price = coin_data[0][4]
     print("Here are the current prices for bitcoin and etherium")
     print("Please wait, generating your response...")
-    eth_price = eth_data[0][4]
     coin_prompt = f"\n\n{coin}-USD historical data:\n- Opens: {coin_opens}\n- Highs: {coin_highs}\n- Lows: {coin_lows}\n- Closes: {coin_closes}"
-    eth_prompt = f"\n\nETH-USD historical data:\n- Opens: {eth_opens}\n- Highs: {eth_highs}\n- Lows: {eth_lows}\n- Closes: {eth_closes}"
-    prompt = f"{coin} coinin gecmis verilere gore bir sonraki gunun fiyati artis egilimde mi dusus mu trendleri cizerek soyleyebilir misin?  {option} investment strategy. gecmis fiyat bilgileri burda 90 gunluktur : {coin_prompt}{eth_prompt}\n\n{coin}-USD price: {coin_price}\n\nETH-USD price: {eth_price}\n\n"
+    prompt = f"{coin} coinin gecmis verilere gore gun mumunda destek ve direnc fiyatini soyleyebilir misin ?  {option} investment strategy. gecmis fiyat bilgileri burda 90 gunluktur : {coin_prompt}\n\n{coin}-USD live price: {coin_price}\n\n"
     print(prompt)
     body = {
         "prompt": prompt,
@@ -39,12 +36,16 @@ def get_investment_recommendation(coin, coin_data, eth_data, coin_opens, coin_hi
     choices = result.get('choices', [])
     completion = choices[0]['text'].strip() if choices else ''
     print(completion)
-    while True:
-        new_prompt = input("Enter your message (or 'exit' to quit): ")
-        prompt += new_prompt
-        print(prompt)
-        completion = chat_with_gpt4(prompt, url, headers=headers)
-        print(completion)
-        if prompt.lower() == 'exit':
-            break
-    return completion
+    choise = input("Do you want to continue? (y/n): ")
+    if choise == 'n':
+        return completion
+    else:
+        while True:
+            new_prompt = input("Enter your message (or 'exit' to quit): ")
+            prompt += new_prompt
+            print(prompt)
+            completion = chat_with_gpt4(prompt, url, headers=headers)
+            print(completion)
+            if prompt.lower() == 'exit':
+                break
+        return completion
