dc327f6031800d22debffd9e0a7dcac45641bbd6
ognn <ogunozalp1991@gmail.com>
2023-07-16 16:50:57 +0300

    sadas

--- modules/investment_recommendation.py
+++ modules/investment_recommendation.py
@@ -1,14 +1,12 @@
 import requests
 
 
-def interact_with_openai(prompt, url, headers):
-
+def chat_with_gpt4(prompt, url, headers):
         body = {
             "prompt": prompt,
             "max_tokens": 585,
             "temperature": 0.1
         }
-
         response = requests.post(url, headers=headers, json=body)
         result = response.json()
         choices = result.get('choices', [])
@@ -29,7 +27,7 @@ def get_investment_recommendation(coin, coin_data, eth_data, coin_opens, coin_hi
     eth_price = eth_data[0][4]
     coin_prompt = f"\n\n{coin}-USD historical data:\n- Opens: {coin_opens}\n- Highs: {coin_highs}\n- Lows: {coin_lows}\n- Closes: {coin_closes}"
     eth_prompt = f"\n\nETH-USD historical data:\n- Opens: {eth_opens}\n- Highs: {eth_highs}\n- Lows: {eth_lows}\n- Closes: {eth_closes}"
-    prompt = f"{coin} coinin gecmis verilere gore bir sonraki gunun fiyati artis egilimde mi dusus mu trendleri cizerek soyleyebilir misin?  {option} investment strategy. gecmis fiyat bilgileri burda 90 gunluktur : {coin_prompt}{eth_prompt}\n\n{coin}-USD price: {coin_price}\nETH-USD price: {eth_price}"
+    prompt = f"{coin} coinin gecmis verilere gore bir sonraki gunun fiyati artis egilimde mi dusus mu trendleri cizerek soyleyebilir misin?  {option} investment strategy. gecmis fiyat bilgileri burda 90 gunluktur : {coin_prompt}{eth_prompt}\n\n{coin}-USD price: {coin_price}\n\nETH-USD price: {eth_price}\n\n"
     print(prompt)
     body = {
         "prompt": prompt,
@@ -42,8 +40,10 @@ def get_investment_recommendation(coin, coin_data, eth_data, coin_opens, coin_hi
     completion = choices[0]['text'].strip() if choices else ''
     print(completion)
     while True:
-        prompt = input("Enter your message (or 'exit' to quit): ")
-        completion = interact_with_openai(prompt, url, headers=headers)
+        new_prompt = input("Enter your message (or 'exit' to quit): ")
+        prompt += new_prompt
+        print(prompt)
+        completion = chat_with_gpt4(prompt, url, headers=headers)
         print(completion)
         if prompt.lower() == 'exit':
             break
