62d6b55e2e1c9fccb1c410628b27c377b9e64de9
ognn <ogunozalp1991@gmail.com>
2023-09-11 01:29:26 +0300

    sadas

--- modules/option.py
+++ modules/option.py
@@ -7,5 +7,5 @@ from modules.investment_recommendation import get_investment_recommendation
 def user_option(coin, coin_data, coin_opens, coin_highs, coin_lows, coin_closes, gpt_api, option, coin_date,day_prediction,coin_orders):
     investment_recommendation = get_investment_recommendation(coin, coin_data, coin_opens, coin_highs, coin_lows,coin_closes, gpt_api, option, coin_date,day_prediction,coin_orders)
 
-    print("\nInvestment Recommendation:", investment_recommendation)
+    # print("\nInvestment Recommendation:", investment_recommendation)
     return
